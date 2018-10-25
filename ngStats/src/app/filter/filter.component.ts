d0488d270c78addd492fd3a6eceb930de5cf9f87
Olivia Liepa <olivialiepa@wifi-roaming-128-4-130-8.host.udel.edu>
2018-10-25 15:15:51 -0400

    combined w/ scss

--- ngStats/src/app/filter/filter.component.ts
+++ ngStats/src/app/filter/filter.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
 
