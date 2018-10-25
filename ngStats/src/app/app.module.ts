7c417628d0e5825fd26bcf66bb440ee63173f63c
Olivia Liepa <olivialiepa@Olivias-MacBook-Pro.local>
2018-10-25 16:10:57 -0400

    upgrade to angular 7 and scss

--- ngStats/src/app/app.module.ts
+++ ngStats/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BarGraphComponent } from './bar-graph/bar-graph.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material';
-import { AppRoutingModule } from './app-routing.module';
+
+
 import 'hammerjs';
-import {MatSliderModule} from '@angular/material/slider';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+
 import { AppComponent } from './app.component';
 import { QuestionTableComponent } from './question-table/question-table.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MaterialModule } from './material.module';
+
 import { MatTableModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
+
 import { TableService } from './table-service.service';
-<<<<<<< HEAD
-=======
 import { AppRoutingModule } from './app-routing.module';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -24,7 +22,6 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { MaterialModule } from './material.module';
->>>>>>> oliepa
 import { FilterComponent } from './filter/filter.component';
 
 
@@ -34,22 +31,13 @@ import { FilterComponent } from './filter/filter.component';
     AppComponent,
     BarGraphComponent,
     QuestionTableComponent,
-<<<<<<< HEAD
-    FilterComponent 
-=======
     FilterComponent
->>>>>>> oliepa
   ],
   imports: [
     ChartsModule,
     BrowserModule,
     BrowserAnimationsModule,
-
     MatTableModule,
-<<<<<<< HEAD
-    MatCardModule,
-=======
->>>>>>> oliepa
     HttpClientModule,
     AppRoutingModule,
     MaterialModule,
--- ngStats/src/app/filter/filter.component.ts
+++ ngStats/src/app/filter/filter.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./filter.component.css']
-=======
   styleUrls: ['./filter.component.scss']
->>>>>>> oliepa
 })
 export class FilterComponent implements OnInit {
 
